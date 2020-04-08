bf872031cd252dfdaa0939119e0f804738d8294a
Jonic Linley <jonic@100yen.co.uk>
2020-04-08 20:16:08 +0100

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,21 @@
-import '@storybook/addon-console'
-
 import { addDecorator } from '@storybook/react'
 import { addParameters } from '@storybook/react'
 import { withA11y } from '@storybook/addon-a11y'
 import { withHTML } from '@whitespace/storybook-addon-html/react'
 import { withPlayroom } from 'storybook-addon-playroom'
 
+// Playroom
+addDecorator(withPlayroom)
+addParameters({
+  playroom: {
+    url: 'http://localhost:9000', // your Playroom URL (default)
+  },
+})
+
+// Accessibility
 addDecorator(withA11y)
+
+// HTML Output
 addDecorator(
   withHTML({
     prettier: {
@@ -16,8 +25,8 @@ addDecorator(
     },
   })
 )
-addDecorator(withPlayroom)
 
+// Design Tokens
 const scssReq = require.context('!!raw-loader!../src/tokens', true, /.\.scss$/)
 const scssTokenFiles = scssReq
   .keys()
