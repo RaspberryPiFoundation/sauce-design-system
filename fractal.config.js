33de539547a79cfdb3439eda859f4954ac2e59bf
Jonic Linley <jonic@100yen.co.uk>
2021-06-22 17:31:16 +0100

    Add Fractal theme

---
+++ fractal.config.js
@@ -0,0 +1,32 @@
+const fractal = require('@frctl/fractal')
+const pkg = require('./package.json')
+
+const sauce = fractal.create()
+const sauceTheme = require('./fractal.theme')
+
+sauce.set('project.title', 'Sauce Design System')
+sauce.set('project.version', pkg.version)
+sauce.set('project.author', pkg.author)
+
+sauce.components.set('default.context', {
+  display: {
+    'max-width': '400px',
+    'min-width': '250px',
+  },
+})
+sauce.components.set('default.status', 'wip')
+sauce.components.set('default.preview', '@preview')
+sauce.components.set('path', `${__dirname}/components`)
+
+sauce.docs.set('indexLabel', 'Home')
+sauce.docs.set('path', `${__dirname}/docs`)
+
+sauce.web.set('builder.dest', `${__dirname}/dist`)
+sauce.web.set('builder.ext', '.html')
+sauce.web.set('builder.urls.ext', null)
+sauce.web.set('server.sync', true)
+sauce.web.set('server.port', 31415)
+sauce.web.set('static.path', `${__dirname}/public`)
+sauce.web.theme(sauceTheme)
+
+module.exports = sauce
---
+++ fractal.theme.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mandelbrot = require('@frctl/mandelbrot')
+const pkg = require('./package.json')
+
+const sauceTheme = mandelbrot({
+  favicon: '/favicon.svg',
+  information: [
+    {
+      label: '<b>Version</b>',
+      value: pkg.version,
+    },
+    {
+      label: '<b>Built on</b>',
+      value: new Date(),
+      type: 'time',
+      format: (value) => value.toISOString().split('T')[0],
+    },
+  ],
+  nav: ['search', 'docs', 'components', 'information'],
+  panels: ['html', 'view', 'context', 'resources', 'info', 'notes'],
+  skin: 'maroon',
+  styles: [
+    'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&amp;family=Roboto+Slab:wght@400;700&amp;family=Space+Mono&amp;display=swap',
+    'default',
+    '/fractal.css',
+  ],
+  static: {
+    mount: 'theme',
+  },
+})
+sauceTheme.addLoadPath(`${__dirname}/theme/views`)
+// sauceTheme.addStatic(`${__dirname}/theme/assets`)
+
+module.exports = sauceTheme
