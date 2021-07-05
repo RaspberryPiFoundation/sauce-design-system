fcaef03261dfb728fa7f08383fa8ecb4d186ea69
Jonic Linley <jonic@100yen.co.uk>
2021-07-05 17:03:16 +0100

    Update panels config

--- fractal.theme.js
+++ fractal.theme.js
@@ -18,15 +18,18 @@ const sauceTheme = mandelbrot({
       format: (value) => value.toISOString().split('T')[0],
     },
   ],
+  labels: {
+    panels: {
+      html: 'HTML',
+      resources: 'Resources',
+      notes: 'Usage Docs',
+      // view: 'Template Example',
+      // context: 'Props',
+      info: 'Info',
+    },
+  },
   nav: ['search', 'docs', 'components', 'information'],
-  panels: [
-    'html',
-    // 'view',
-    // 'context',
-    'resources',
-    'notes',
-    'info',
-  ],
+  panels: ['html', 'resources', 'notes', 'view', 'context', 'info'],
   skin: 'maroon',
   styles: [
     'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&amp;family=Roboto+Slab:wght@400;700&amp;family=Space+Mono&amp;display=swap',
