65759b9f1f1e64c40ef327c4d0b3a8f2aef8e60b
Jonic Linley <jonic@100yen.co.uk>
2021-10-12 16:35:16 +0100

    Update theme config

--- fractal.theme.js
+++ fractal.theme.js
@@ -18,24 +18,20 @@ const sauceTheme = mandelbrot({
       format: (value) => value.toISOString().split('T')[0],
     },
   ],
-  labels: {
-    panels: {
-      html: 'HTML',
-      resources: 'Resources',
-      notes: 'Usage Docs',
-      // view: 'Template Example',
-      // context: 'Props',
-      info: 'Info',
-    },
-  },
   nav: ['search', 'docs', 'components', 'information'],
   panels: ['html', 'resources', 'notes', 'view', 'context', 'info'],
+  scripts: ['default', '/theme/theme-switcher.js'],
   skin: 'maroon',
-  styles: [
-    'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&amp;family=Roboto+Slab:wght@400;700&amp;family=Space+Mono&amp;display=swap',
-    'default',
-    '/theme/fractal.css',
-  ],
+  styles: ['default', '/theme/theme-switcher.css'],
+  themes: {
+    'raspberry-pi': 'Raspberry Pi',
+    'astro-pi': 'Astro Pi',
+    'code-club-world': 'Code Club World',
+    'code-club': 'Code Club',
+    coderdojo: 'CoderDojo',
+    'coolest-projects': 'Coolest Projects',
+    'hello-world': 'Hello World',
+  },
 })
 sauceTheme.addLoadPath(`${__dirname}/theme/views`)
 sauceTheme.addStatic(`${__dirname}/theme/assets`, '/theme')
